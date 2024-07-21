fe652cf24ce5e38ed43e8997733717c529317c72
DavudKarimov <davudkerimov1453@gmail.com>
2024-07-21 04:06:08 +0400

    Initial setup of e-commerce project: integrated Bootstrap, created admin page, set up model page, and added Pillow library
    - Created a new Django project for the e-commerce application.
    - Integrated Bootstrap for responsive design.
    - Set up the Django admin interface to manage the application.
    - Created initial models for the e-commerce platform.- Added Pillow library for image processing.

---
+++ ecom/store/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+import datetime
+
+# Categories of Products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+# Customers
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+# All of our Products
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0) 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+
+
+# Customer Orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', null=True, blank=True)
+    phone = models.CharField(max_length=20, default='', null=True, blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
